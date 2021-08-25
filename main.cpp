c35c051f3680f618c76e40f17a0794ec2720f00a
phamngoclam2628 <84197015+phamngoclam2628@users.noreply.github.com>
2021-08-25 09:01:03 +0000

    update code

--- main.cpp
+++ main.cpp
@@ -1,5 +1,42 @@
 #include <iostream>
 
+using namespace std;
+
+class hangHoa{
+public:
+  char maHang[2];
+  char tenHang[20];
+  char donViTinh[10];
+  float donGia, soLuong, thanhTien;
+
+  void Input();
+};
+
+void hangHoa::Input(){
+  cout << "Nhap ma hang: "; cin >> maHang;
+  cout << "Nhap ten hang: "; cin >> tenHang;
+  cout << "Nhap don vi tinh: "; cin >> donViTinh;
+  cout << "Nhap don gia: "; cin >> donGia;
+  cout << "Nhap so luong: "; cin >> soLuong;
+}
+
+class newHangHoa : public hangHoa {
+public:
+  float donGiaVanChuyen, congVanChuyen;
+
+  void tinhCongVanChuyen();
+  void tinhThanhTien();
+};
+
+void newHangHoa::tinhCongVanChuyen(){
+
+}
+
+void newHangHoa::tinhThanhTien(){
+  
+}
+
+
 int main() {
   std::cout << "Hello World!\n";
 }
